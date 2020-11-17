0c6025276e82c3da570ca68e8fe9ffc5b5e58845
unknown <abdul.daim26@gmail.com>
2020-11-17 10:37:43 +0000

    Some changes made to login and Auth session

---
+++ Server/src/main/java/microservices/Spring/Controllers/LoginController.java
@@ -0,0 +1,21 @@
+package microservices.Spring.Controllers;
+
+
+import microservices.Spring.classes.Login;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+public class LoginController {
+
+    @CrossOrigin("http://localhost:5000")
+    @PostMapping(path = "/login", consumes = "application/json", produces = "application/json")
+    Login login(@RequestBody Login login){
+        return login;
+    }
+
+}
---
+++ Server/src/main/java/microservices/Spring/classes/Login.java
@@ -0,0 +1,12 @@
+package microservices.Spring.classes;
+
+public class Login {
+    public String email;
+    public String password;
+
+    public Login(String email, String password){
+        this.email = email;
+        this.password = password;
+
+    }
+}
